099246b765667c3cf3e67027323bd9e862c14cae
Piotr Dobiech <pitcer@protonmail.com>
2019-08-08 02:33:45 +0200

    WIP: Add toml4j module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 rootProject.name = extra.properties["name"] as String
 
-include("unifig-core", "unifig-gson", "unifig-paper")
+include("unifig-core", "unifig-gson", "unifig-paper", "unifig-toml4j")
---
+++ unifig-toml4j/build.gradle.kts
@@ -0,0 +1,4 @@
+dependencies {
+	api(project(":unifig-core"))
+	implementation("com.moandjiezana.toml:toml4j:0.7.2")
+}
