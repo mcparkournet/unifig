96548de811e3cf7dc620dda1585d991e19248774
Piotr Dobiech <pitcer@protonmail.com>
2020-01-10 20:55:44 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,11 +20,11 @@ subprojects {
 	}
 
 	dependencies {
-		compileOnly("org.jetbrains:annotations:17.0.0")
+		compileOnly("org.jetbrains:annotations:18.0.0")
 		testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
 		testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
-		testCompileOnly("org.jetbrains:annotations:17.0.0")
-		testFixturesCompileOnly("org.jetbrains:annotations:17.0.0")
+		testCompileOnly("org.jetbrains:annotations:18.0.0")
+		testFixturesCompileOnly("org.jetbrains:annotations:18.0.0")
 	}
 
 	java {
--- unifig-kotlin/build.gradle.kts
+++ unifig-kotlin/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-	kotlin("jvm") version "1.3.50"
+	kotlin("jvm") version "1.3.61"
 }
 
 dependencies {
 	api(project(":unifig-core"))
-	implementation(kotlin("stdlib-jdk8", "1.3.50"))
+	implementation(kotlin("stdlib-jdk8", "1.3.61"))
 }
 
 tasks {
